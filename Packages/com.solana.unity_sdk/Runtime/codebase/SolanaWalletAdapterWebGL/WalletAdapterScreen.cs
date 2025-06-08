78cd659e909b86402149eea734e101d9a7fd5d28
Luka Markovic <45857004+LukaMarkovic8@users.noreply.github.com>
2025-06-08 16:37:39 +0200

    Add backpack and phantom wallets

--- Packages/com.solana.unity_sdk/Runtime/codebase/SolanaWalletAdapterWebGL/WalletAdapterScreen.cs
+++ Packages/com.solana.unity_sdk/Runtime/codebase/SolanaWalletAdapterWebGL/WalletAdapterScreen.cs
@@ -8,14 +8,14 @@ using UnityEngine.UI;
 
 namespace Solana.Unity.SDK
 {
-   
-    public class WalletAdapterScreen: MonoBehaviour
+
+    public class WalletAdapterScreen : MonoBehaviour
     {
         public GameObject buttonPrefab;
         public RectTransform viewPortContent;
         public Action<string> OnSelectedAction;
         private HashSet<string> _addedWallets = new();
-        
+
 
         private void OnEnable()
         {
@@ -24,10 +24,8 @@ namespace Solana.Unity.SDK
 
         private void _createWalletAdapterButton(SolanaWalletAdapterWebGL.WalletSpecs wallet)
         {
-            if(wallet.name != "Solflare")
-            {
-                return;
-            }
+            if (wallet.name == "Solflare" || wallet.name == "Backpack" || wallet.name == "Phantom") { }
+            else { return; }
 
             var g = Instantiate(buttonPrefab, viewPortContent);
             var walletButton = g.GetComponent<WalletAdapterButton>();
@@ -35,7 +33,7 @@ namespace Solana.Unity.SDK
             walletButton.Name = wallet.name;
             walletButton.DetectedLabel.GetComponent<TextMeshProUGUI>().enabled = wallet.installed;
             walletButton.OnSelectedAction = walletName =>
-            { 
+            {
                 OnSelectedAction?.Invoke(walletName);
             };
             Texture2D tex = new Texture2D(2, 2);
@@ -45,22 +43,22 @@ namespace Solana.Unity.SDK
             walletButton.Icon.GetComponent<Image>().sprite = iconSprite;
         }
         private void UpdateWalletAdapterButtons()
-         {
-             foreach (var wallet in SolanaWalletAdapterWebGL.Wallets)
-             {
-                 if (_addedWallets.Contains(wallet.name))
-                 {
-                     continue;  
-                 }
-                 _addedWallets.Add(wallet.name);
-                 _createWalletAdapterButton(wallet);
-             }
-         }
-         
-         public void OnClose()
-         {
-             transform.parent.gameObject.SetActive(false);
-         }
+        {
+            foreach (var wallet in SolanaWalletAdapterWebGL.Wallets)
+            {
+                if (_addedWallets.Contains(wallet.name))
+                {
+                    continue;
+                }
+                _addedWallets.Add(wallet.name);
+                _createWalletAdapterButton(wallet);
+            }
+        }
+
+        public void OnClose()
+        {
+            transform.parent.gameObject.SetActive(false);
+        }
 
     }
 }
